e09c1851263be4f970b5635221c240415dd85a57
Raphael Pour <info@raphaelpour.de>
2026-07-14 09:27:07 +0200

    feat: implement fmt.Stringer interface
    Implement  to satisfy fmt.Stringer interface.
    Always use IEC units (base 2).

--- example_test.go
+++ example_test.go
@@ -6,10 +6,23 @@ import (
 	"github.com/raphaelpour/size"
 )
 
-func ExampleSize() {
+func ExampleSize_Bytes() {
 	blockSize := 4 * size.Mebibyte
 	fmt.Printf("%d", blockSize.Bytes())
 
 	// Output:
 	// 4194304
+}
+
+func ExampleSize_String() {
+	fmt.Println(42 * size.Gib)
+	fmt.Println(42 * size.Gb)
+	fmt.Println(1024 * size.Kib)
+	fmt.Println(1024 * size.Kb)
+
+	// Output:
+	// 42Gib
+	// 39.12Gib
+	// 1Mib
+	// 1000Kib
 }
